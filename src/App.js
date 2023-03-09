03b59bb6fdb29237d89f42d2b03a7ee945a653b2
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-03-09 06:29:57 -0300

    Primeiro commit 09/03/23

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import './style.css';
 
 ///https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
   const [nutri, setNutri] = useState([]);
 
+  useEffect(() => {
+
+    function loadApi(){
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+      fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        console.log(json);
+        setNutri(json);
+      })
+    }
+
+    loadApi();
+
+  }, []);
 
   return (
-    <div>
-      <h1>TESTE</h1>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
     </div>
   );
 }
