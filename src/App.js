d9f49e5dd4e91202626c56483843043e8662cea5
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-03-10 18:57:58 -0300

    Segundo commit 10/03/23

--- src/App.js
+++ src/App.js
@@ -1,56 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import './style.css';
-
-///https://sujeitoprogramador.com/rn-api/?api=posts
-
-function App() {
-  const [nutri, setNutri] = useState([]);
-
-  useEffect(() => {
-
-    function loadApi(){
-      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
-
-      fetch(url)
-      .then((r) => r.json())
-      .then((json) => {
-        console.log(json);
-        setNutri(json);
-      })
-    }
-
-    // Chamar a função!!!       
-    loadApi();
-
-  }, []);
-
-  return (
-    <div className="container">
-      <header>
-        <strong>React Nutri</strong>
-      </header>
-
-      {nutri.map((item) => {
-        return(
-          <article key={item.id} className="post">
-            <strong className="titulo">{item.titulo}</strong>
-
-            <img src={item.capa} alt={item.titulo} className="capa" />
-            <p className="subtitulo">
-              {item.subtitulo}
-            </p> 
-            <a className="botao">Acessar</a>
-          </article>
-        )
-      })}
-
+export default function App() {
+  return(
+    <div>
+      <h1>Rotas com React Js</h1>
     </div>
   );
 }
-
-export default App;
-
-
-
-
-
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Routes = () => {
+    
+}
