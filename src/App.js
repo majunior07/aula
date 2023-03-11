b3e484bef3267ddac74fa9638225639d57ae7230
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-03-11 11:31:24 -0300

    Primeiro commit 11/03/23

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Routes from "./routes";
+
 export default function App() {
   return(
-    <div>
-      <h1>Rotas com React Js</h1>
-    </div>
+      <Routes/>      
   );
 }
+
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,20 @@
 import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Contato from './pages/Contato';
+
+import Header from './components/Header';
 
 const Routes = () => {
-    
+    return(
+        <BrowserRouter>          
+           <switch>
+                <Route exact path="/" component={Home} /> 
+                <Route path="/sobre" component={Sobre} />
+                <Route path="/contato" component={Contato} />
+           </switch>
+        </BrowserRouter>
+    )
 }
+
+export default Routes;
